d06ec1e13c87c0b71bd5fdb9495f78aaf83ff1f0
duongnartist <duongnartist@gmail.com>
2016-01-28 02:32:07 +0700

    first commit

---
+++ PoemReader/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  PoemReader
+//
+//  Created by DuongNArtist on 1/27/16.
+//  Copyright (c) 2016 DuongNArtist. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
